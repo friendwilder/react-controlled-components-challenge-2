6ea5a3dbc2108052ae65a152eb1bb07fea561258
Wilder Sánchez <friendwilder@yahoo.com.mx>
2021-03-03 16:44:03 -0600

    Added fetch inside componentDidMount

--- src/components/PokeCards.js
+++ src/components/PokeCards.js
@@ -4,20 +4,47 @@ class PokeCards extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayed_picture: "default",
-      test_string: ""
+      loaded: "false",
+      pokemonsData: [],
+      testString: ""
     };
     this.handleMouseOver = this.handleMouseOver.bind(this);
   }
+
+  componentDidMount() {
+    console.log("Component did mount");
+    fetch("pokeData.js")
+      .then((response) => response.json())
+      .then((result) => {
+        const pokemonsData = result.map((pokemon) => {
+          return pokemon;
+        });
+        this.setState({
+          // It never gets to the setState because when fetching the file
+          // it is not being processed correctly. Using a txt works.
+          pokemonsData: pokemonsData,
+          loaded: "true"
+        });
+      });
+  }
+
   handleMouseOver = (event) => {
-    console.log(event);
+    console.log(this.state.loaded);
+    fetch("pokeData.js")
+      .then((response) => response.text())
+      .then((text) => {
+        // console.log(text);
+      });
     this.setState({
-      displayed_picture: "shiny"
+      displayedPicture: "shiny"
     });
   };
 
   render() {
     // console.log(this.props);
+    const listPokemons = this.state.pokemonsData.map((pok) => (
+      <div>pok.name</div>
+    ));
     const mockPokemonData = this.props.mockPokemonData.sort();
 
     let my_img;
@@ -33,6 +60,7 @@ class PokeCards extends Component {
     // console.log(my_default)
     return (
       <div>
+        <div>{listPokemons}</div>
         <ol className="pokemon-list">
           {mockPokemonData.map((pokemon, index) => (
             <li key={pokemon.name} className="pokemon-list-item">
