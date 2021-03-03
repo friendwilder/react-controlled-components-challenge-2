6e136416fc3f9ef3ceaf53094e3e15bfbc201696
Wilder Sánchez <friendwilder@yahoo.com.mx>
2021-03-03 17:40:12 -0600

    Added onMouseOver and OnMouseOut event handlers

--- src/components/PokeCards.js
+++ src/components/PokeCards.js
@@ -3,64 +3,14 @@ import React, { Component } from "react";
 class PokeCards extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      loaded: "false",
-      pokemonsData: [],
-      testString: ""
-    };
-    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.state = {};
   }
-
-  componentDidMount() {
-    console.log("Component did mount");
-    fetch("pokeData.js")
-      .then((response) => response.json())
-      .then((result) => {
-        const pokemonsData = result.map((pokemon) => {
-          return pokemon;
-        });
-        this.setState({
-          // It never gets to the setState because when fetching the file
-          // it is not being processed correctly. Using a txt works.
-          pokemonsData: pokemonsData,
-          loaded: "true"
-        });
-      });
-  }
-
-  handleMouseOver = (event) => {
-    console.log(this.state.loaded);
-    fetch("pokeData.js")
-      .then((response) => response.text())
-      .then((text) => {
-        // console.log(text);
-      });
-    this.setState({
-      displayedPicture: "shiny"
-    });
-  };
 
   render() {
-    // console.log(this.props);
-    const listPokemons = this.state.pokemonsData.map((pok) => (
-      <div>pok.name</div>
-    ));
     const mockPokemonData = this.props.mockPokemonData.sort();
 
-    let my_img;
-    if (this.state.displayed_picture === "default") {
-      my_img = (
-        <img
-          alt="Pokemon default"
-          onMouseOver={this.handleMouseOver}
-          src={this.props.mockPokemonData}
-        />
-      );
-    }
-    // console.log(my_default)
     return (
       <div>
-        <div>{listPokemons}</div>
         <ol className="pokemon-list">
           {mockPokemonData.map((pokemon, index) => (
             <li key={pokemon.name} className="pokemon-list-item">
@@ -69,10 +19,17 @@ class PokeCards extends Component {
                 <a href={pokemon.videoLink} target="_blank" rel="noreferrer">
                   <img
                     alt={pokemon.name}
-                    onMouseOver={() => this.handleMouseOver(index)}
+                    onMouseOver={(event) =>
+                      (event.currentTarget.src = pokemon.sprites.front_shiny)
+                    }
                     src={pokemon.sprites.front_default}
+                    onMouseOut={(event) =>
+                      (event.currentTarget.src = pokemon.sprites.front_default)
+                    }
+                    onError={(event) =>
+                      (event.currentTarget.src = pokemon.sprites.back_default)
+                    }
                   />
-                  <img alt="Prueba" src={pokemon.sprites.front_shiny} />
                 </a>
               </div>
             </li>
